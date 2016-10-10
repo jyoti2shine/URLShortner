733d07f0b940f20ec12b886149c3654c423d1471
jyoti-sumeru <jyoti.singh@sumerusolutions.com>
2016-10-10 21:55:03 +0530

    Added changes in rspec

--- spec/models/short_url_spec.rb
+++ spec/models/short_url_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe ShortUrl, type: :model do
   it { should validate_presence_of(:original_url) }
+  it { should belong_to(:user) }
+  it { should have_one(:short_visit) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
+  it { should have_many(:short_urls) }
 end
